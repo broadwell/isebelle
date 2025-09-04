6a29afdb3969e2ee3445724c28a0bce008eb1d35
Peter Broadwell <broadwell@stanford.edu>
2025-09-03 20:53:18 -0700

    Needed for dynamic content on browse page

---
+++ web-ui/src/routes/browse/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+
+export const prerender = 'auto';
+
+export const ssr = false;
+
+export const load: PageLoad = async ({ params }) => {
+
+    return params;
+
+}
